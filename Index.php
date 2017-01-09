bb122eecae5e875cf783827cffca7bc61e1b28c7
EMartinez <emartinez@grupohi.mx>
2017-01-09 14:19:33 -0600

    2017.2.0.2: Corrección de módulo para revertir viajes

--- Index.php
+++ Index.php
@@ -100,7 +100,7 @@
                                                 <a href="Logout.php">
                                                 <button>Cerrar Sesi&oacute;n</button>                                                    
                                                 </a>
-                                                2017.2.0.1
+                                                2017.2.0.2
                                             </div>
                                         </td>
                                     </tr>
